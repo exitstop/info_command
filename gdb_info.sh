ce6ba2eef8240ad3f41c324cea6063b73eca1671
user <exitstop@list.ru>
2018-08-02 10:22:19 +0300

    add command gdb

--- gdb_info.sh
+++ gdb_info.sh
@@ -1,3 +1,15 @@
 #открыть исходник при отладке
 ctrl+x a
 
+#перенаправляем dashboard в другой терминал по tty - его можно узнать с помощью команды tty
+dashboard -output /dev/pts/2
+
+#меняем размер окна source 
+dashboard source -style context 17
+
+#оставляем включенным только некоторые окна
+dashboard -layout source stack threads
+
+#посмотреть динамические библиотеки приложения
+i shared
+
