d7b1f32f2094a212ae2b307a6cf550668d77bd9e
user <exitstop@list.ru>
2018-08-04 20:59:29 +0300

    delete empty string

--- bash_info.sh
+++ bash_info.sh
@@ -2,37 +2,27 @@
 #one(if=one) начиная со смещения 0(skip=0) 
 #и записать в файл two(of=two) начиная со смещения 1(seek=1) 
 #и не затирать конец файла two (conv=notrunc)
-
 dd bs=1 count=5 if=one of=two skip=0 seek=1 conv=notrunc
-
 #conver webm to mpr
 FILE="the-file-you-want-to-process.webm";
 ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";
-
 #тоже самое но в цикле
 for FILE in *.webm; do
     echo -e "Processing video '\e[32m$FILE\e[0m'";
     ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";
 done;
-
 #посмотреть динамические библиотеки забущенного процесса
 lsof -p <pid>
-
 #удалить имена функций из программы 
 strip program
-
 #посмотреть имена функций которые есть в программе
 nm program
-
 #установить ssh и включить
 sudo apt install ssh
 sudo systemctl enable ssh
-
 #рекурсиваня замена во всех файлах
 a=hello&&b=hello2&& grep -rl "$a" | xargs sed -i "s/$a/$b/g"
-
 #посмотреть сколько места в системе
 df -h
-
 #посмотерть сколько весят папки и файлы в текущей дериктории и отсортировать
 du -hs * | sort -h
--- gdb_info.sh
+++ gdb_info.sh
@@ -1,15 +1,10 @@
 #открыть исходник при отладке
 ctrl+x a
-
 #перенаправляем dashboard в другой терминал по tty - его можно узнать с помощью команды tty
 dashboard -output /dev/pts/2
-
 #меняем размер окна source 
 dashboard source -style context 17
-
 #оставляем включенным только некоторые окна
 dashboard -layout source stack threads
-
 #посмотреть динамические библиотеки приложения
 i shared
-
