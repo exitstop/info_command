66b67393571984ae8c05eb58dfdb4c9d714868e4
user <exitstop@list.ru>
2018-08-06 22:18:10 +0300

    gdb -args

--- gdb_info.sh
+++ gdb_info.sh
@@ -1,5 +1,7 @@
 #открыть исходник при отладке
 ctrl+x a
+#запусть программу с параметрами
+gdb -args ./prog param1 param2
 #перенаправляем dashboard в другой терминал по tty - его можно узнать с помощью команды tty
 dashboard -output /dev/pts/2
 #меняем размер окна source 
