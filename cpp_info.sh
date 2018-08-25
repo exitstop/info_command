f32fab3b2b47bb528cdd6b50e04cc360384dfc40
user <exitstop@list.ru>
2018-08-25 22:12:11 +0300

    profiler c++

--- cpp_info.sh
+++ cpp_info.sh
@@ -1,1 +1,9 @@
 c++filt _ZN5Abase3getEv # convert to -> Abase::get()
+
+#profile c++
+# build the program (no special flags are needed)
+g++ -std=c++11 cpuload.cpp -o cpuload
+# run the program with callgrind; generates a file callgrind.out.12345 that can be viewed with kcachegrind
+valgrind --tool=callgrind ./cpuload
+# open profile.callgrind with kcachegrind
+kcachegrind profile.callgrind
