233de604928bb1e77cd68fb6731c4d5eb00eada6
user <exitstop@list.ru>
2018-08-31 23:40:15 +0300

    gdb

--- gdb_info.sh
+++ gdb_info.sh
@@ -10,3 +10,14 @@ dashboard source -style context 17
 dashboard -layout source stack threads
 #посмотреть динамические библиотеки приложения
 i shared
+#чтобы изменения были постоянными
+mkdir -p ~/.gdbinit.d
+dashboard -configuration ~/.gdbinit.d/auto
+
+s # шаг внутрь функции
+n # шаг через функцию
+l # показать листинг
+l +50 # показать +50 строчек от текущей
+i b # показать break point list
+del 1 # удалить break point number 1
+b ./source/main.cpp:58 # поставить break point на строке 58 в файле, файл может не находится если выполнилась до этого файла
