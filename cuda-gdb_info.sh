6272e99d9f1d658bf61d2c1652b58b4a468fe80a
user <exitstop@list.ru>
2018-09-01 00:52:01 +0300

    cuda-gdb

---
+++ cuda-gdb_info.sh
@@ -0,0 +1,3 @@
+# чтобы профилировать на одном gpu при возникновении ошибки:
+#fatal:  All CUDA devices are used for display and cannot be used while debugging. (error code = CUDBG_ERROR_ALL_DEVICES_WATCHDOGGED(0x18)
+set cuda software_preemption on
