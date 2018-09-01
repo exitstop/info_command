19c52524198f0ce307985968b5e5b645ed78e556
user <exitstop@list.ru>
2018-09-01 12:02:47 +0300

    cuda-gdb

--- cuda-gdb_info.sh
+++ cuda-gdb_info.sh
@@ -1,3 +1,7 @@
+# переключение видеокарт
+prime-select query
+sudo prime-select nvidia
+sudo prime-select intel
 # чтобы профилировать на одном gpu при возникновении ошибки:
 #fatal:  All CUDA devices are used for display and cannot be used while debugging. (error code = CUDBG_ERROR_ALL_DEVICES_WATCHDOGGED(0x18)
 set cuda software_preemption on
