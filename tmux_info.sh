f9ddb9666d6355f1bac75f8fd78174c64db12f08
user <exitstop@list.ru>
2018-09-13 11:07:11 +0300

    tmux info

---
+++ tmux_info.sh
@@ -0,0 +1,21 @@
+CTRL+B % # - разделить окно по вертикали
+CTRL+B " #" - разделить окно по горизонтали
+CTRL+B <UP ARROW> # двигаться по окнам
+CTRL+B z # - full-screen
+CTRL+B c # - new window(tab)
+CTRL+B <window number> # - переключится на вкладку с номером
+CTRL+B d - # detach
+CTRL+B w - # list windows
+CTRL+B & - # kill
+CTRL+B , - # name window
+CTRL+B . - # move window - prompted for a new number
+CTRL+B : - # move window to the next unused number
+CTRL+B o # swap panes
+CTRL+B q # show pane numbers
+CTRL+B x # kill pane
+CTRL+B ⍽ # space - toggle between layouts
+CTRL+B :joinp -s :2<CR> move window 2 into a new pane in the current window
+CTRL+B :joinp -t :1<CR> move the current pane into a new pane in window 1
+CTRL+B t # big clock
+CTRL+B ? # list shortcuts
+CTRL+B : # prompt
