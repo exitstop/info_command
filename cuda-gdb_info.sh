a6bbb455033636ec214a60ea6e0a79057ac8d0d3
user <exitstop@list.ru>
2018-09-13 23:33:59 +0300

    cuda gdb

--- cuda-gdb_info.sh
+++ cuda-gdb_info.sh
@@ -5,3 +5,6 @@ sudo prime-select intel
 # чтобы профилировать на одном gpu при возникновении ошибки:
 #fatal:  All CUDA devices are used for display and cannot be used while debugging. (error code = CUDBG_ERROR_ALL_DEVICES_WATCHDOGGED(0x18)
 set cuda software_preemption on
+# чтобы можно было отлаживать нужно сделать в CMakeLists.txt. Возможно -keep не обязателен
+set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS}; --device-debug; -keep)
+
