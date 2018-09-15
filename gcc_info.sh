dabf6782ca77445641762b8552f0d9ca91fc987f
user <exitstop@list.ru>
2018-09-16 00:45:24 +0300

    extern

--- gcc_info.sh
+++ gcc_info.sh
@@ -1,2 +1,9 @@
 gcc -O3 -masm=intel main.cpp -S # компиляция получить asm листинг, с диалектом intel
 gcc -S -masm=intel -O2 libAsm.cpp -I. # превратить *.cpp в Asm листинг -I. нужен чтобы подключить текующую деррикторию  
+
+# ld чтобы библиотека нашлась в текущем каталоге
+export LD_LIBRARY_PATH=.
+export LD_PRELOAD=. # или это
+
+# соглашение об именах
+extern "C" void hello(const int a);
