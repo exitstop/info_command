1e25ab1345c8db47c8dccf4fe3aca7245abee28a
user <exitstop@list.ru>
2018-09-16 11:06:51 +0300

    gdb

--- gdb_info.sh
+++ gdb_info.sh
@@ -21,25 +21,40 @@ dashboard -configuration ~/.gdbinit.d/auto
 r/run # run или рестарт
 c/continue # continue продолжить выполнение
 s/step # шаг внутрь функции
-stepi # ходить по асемблерным иструкциям
-nexti # ходить по асемблерным иструкциям
+si/stepi # ходить по асемблерным иструкциям
+ni/nexti # ходить по асемблерным иструкциям
 n/next # шаг через функцию
 l/list # показать листинг
-finish # выйти из функции
+finish # дойти до конца функции 
+ret # выйти из функции
 kill # убить
 q # выйти из gdb
 l +50 # показать +50 строчек от текущей
 i b # показать break point list
+i proc m # memory maping
 del 1 # удалить break point number 1
+disable 2 # отключить break point 2 но не удалять
 where # показать стек и где мы сейчас находимся
 b ./source/main.cpp:58 # поставить break point на строке 58 в файле ./source/main.cpp
 b ./source/main.cpp:60 if a == 5 # условная точка останова
+cond 1 $rdi==0x0 # изменить брейкпоинт 1 на условный
 bt/backtrace
 f/frame
 backtrace full
 up/down number # перемещение по стеку
 i/info
 info frame
+
+set environment LD_BIND_NOW=1 # если будет ошибка dl-trampoline.h при записи
+record full # чтобы записать пошаговое выполнение а потом перематывать
+record stop # остановить запись
+info record
+record save filename
+record goto start
+record goto end
+reverse-step # шаг назад
+reverse-stepi # шаг назад на одну asm иструкцию
+
 
 #p/print
 #p/(x,o,d,f,c,t)
