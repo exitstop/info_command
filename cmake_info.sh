8995aa3955819a642283a230ca5ae7bbba013e2a
user <exitstop@list.ru>
2018-11-14 12:35:27 +0300

    verbose cmake

--- cmake_info.sh
+++ cmake_info.sh
@@ -1,3 +1,6 @@
 # компилим с использованием всех возможностей нашей системы, но вероятно мы потеряем совместимость с другими процессорами.
 # но при этом может сильно вырасти производительность
-SET_TARGET_PROPERTIES(project PROPERTIES COMPILE_FLAGS "-O3 -march=native")
+set_target_properties(project PROPERTIES COMPILE_FLAGS "-O3 -march=native")
+
+# чтобы сделать verbose output при компиляции. 
+set_target_properties(project PROPERTIES COMPILE_FLAGS "-save-temps")
