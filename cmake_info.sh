aec148b202df957c3d8e22afb6387fe85d8c085f
user <exitstop@list.ru>
2018-11-22 22:56:21 +0300

    GCC Profile-guided optimization

--- cmake_info.sh
+++ cmake_info.sh
@@ -17,3 +17,7 @@ cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pg")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pg")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pg")
+
+# GCC Profile-guided optimization
+--fprofile-generate
+--fprofile-use
