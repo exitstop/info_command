b8c013991fcec23e529f7005599f278534aee7d5
user <exitstop@list.ru>
2018-11-25 17:27:24 +0300

    add ender3

---
+++ ender3_info.sh
@@ -0,0 +1,3 @@
+# если ничего не помогает и ender3 на кристале Atmega 1284p 16 Mhz, не грузит boot loader и выдает ошибку  "initialization failed, rc=-1"
+# чтобы прошить bootloader нужно скачать, ardino 1.0 and install this board https://github.com/JChristensen/mighty-1284p/tree/v1.6.3
+# и выбираем устройство Mighty-1284p и шьем с помошью "arduino as spi"
