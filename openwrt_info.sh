dd009a03973b0f295d8794562d4fe7eceeb78ed8
user <exitstop@list.ru>
2018-12-07 12:37:19 +0300

    opewrt_info.sh

---
+++ openwrt_info.sh
@@ -0,0 +1,5 @@
+# установка gcc с хедерами
+#https://github.com/Entware/Entware-ng/wiki/Using-gcc-(native-compilation)
+
+# opkg install 
+# https://github.com/Entware/Entware/wiki/Install-on-Synology-NAS
