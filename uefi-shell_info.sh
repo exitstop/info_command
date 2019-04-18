2ef2ac25d7150d78c0709dec1ad2afe8d63ba717
user <exitstop@list.ru>
2019-04-18 15:00:58 +0300

    uefi-shell

--- uefi-shell_info.sh
+++ uefi-shell_info.sh
@@ -1,8 +1,10 @@
 # https://wiki.osdev.org/User:No92/Random_Bits
-# https://github.com/tianocore/tianocore.github.io/wiki/Common-instructions
+# Иструция по билду edk2 (нужно работать под bash, а то zsh не тянет) https://github.com/tianocore/tianocore.github.io/wiki/Common-instructions
 # http://www.linux-kvm.org/downloads/lersek/ovmf-whitepaper-c770f8c.txt
 # HelloWorld
 # https://wiki.osdev.org/UEFI
+# compile prject edk2 helloWorld https://www.basicinputoutput.com/2017/06/hello-world-quick-start-with-udk2015.html
+# create makeFile efi https://www.rodsbooks.com/efi-programming/hello.html
 mkdir ~/src
 cd ~/src
 git clone https://github.com/tianocore/edk2
@@ -14,4 +16,7 @@ bash$ . edksetup.sh
 vim Conf/target.txt
 
 cargo install uefi-run
+
+# compile sample HelloWorld
+build -m MdeModulePkg/Application/HelloWorld/HelloWorld.inf
 
