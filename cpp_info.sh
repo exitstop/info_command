47847cb6b9c62cf8fe926c002629674938ba70a6
user <exitstop@list.ru>
2019-04-28 20:48:03 +0300

    valgrind cache

--- cpp_info.sh
+++ cpp_info.sh
@@ -5,8 +5,12 @@ c++filt _ZN5Abase3getEv # convert to -> Abase::get()
 g++ -std=c++11 cpuload.cpp -o cpuload
 # run the program with callgrind; generates a file callgrind.out.12345 that can be viewed with kcachegrind
 valgrind --tool=callgrind ./cpuload
+valgrind --dump-instr=yes --collect-jumps=yes --tool=callgrind ./cpuload
 # open profile.callgrind with kcachegrind
 kcachegrind profile.callgrind
+
+# кэш кеш cache
+valgrind --tool=cachegrind ./prog
 
 # разбор argc
 man 3
