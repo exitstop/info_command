e19b253f296d28e27d40e160466a45d2ce270a21
exitstop <exitstop@list.ru>
2019-06-05 23:56:31 +0300

    nvim add

---
+++ nvim_info.sh
@@ -0,0 +1,5 @@
+# neovim nvim cpp ide config
+https://github.com/optimizacija/neovim-config
+
+# clang 8
+https://solarianprogrammer.com/2017/12/13/linux-wsl-install-clang-libcpp-compile-cpp-17-programs/
