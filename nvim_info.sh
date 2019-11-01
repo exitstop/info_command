c5bf374821e6eb62923cd256b7c54c8b7d530419
exitstop <exitstop@list.ru>
2019-11-01 12:03:30 +0300

    nvim change

--- nvim_info.sh
+++ nvim_info.sh
@@ -3,3 +3,6 @@ https://github.com/optimizacija/neovim-config
 
 # clang 8
 https://solarianprogrammer.com/2017/12/13/linux-wsl-install-clang-libcpp-compile-cpp-17-programs/
+
+# добавить в CMake для ycm youcompleteme появится файл .clang_complete
+set(CMAKE_EXPORT_COMPILE_COMMANDS 1)
