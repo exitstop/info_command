1c5e2b978fa1d037286be917f79f1975c51719e9
Kolos Bogdan <existop@list.ru>
2020-04-28 01:20:25 +0300

    bluetooth

--- lubuntu_info.sh
+++ lubuntu_info.sh
@@ -7,6 +7,14 @@ sudo update-initramfs -u
 Не перезагружается, no reboot
 # Введите sudo vim /etc/default/grub
 # В строке: GRUB_CMDLINE_LINUX_DEFAULT="quiet splash" стереть quiet splash
+# GRUB_CMDLINE_LINUX="reboot=bios"
 # Сохранить и выйти
 # Запустите sudo update-grub
 # Перезагрузиться один раз
+
+# fix blueman
+sudo apt-get install pulseaudio-module-bluetooth
+pactl load-module module-bluetooth-discover
+
+pulseaudio -k
+pulseaudio -D
