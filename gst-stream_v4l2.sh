6a2dd2f770f4fcc5e012ae38be2e6ffeb8fe6ae0
exitstop <exitstop@list.ru>
2020-05-17 23:48:06 +0300

    lsusb

--- gst-stream_v4l2.sh
+++ gst-stream_v4l2.sh
@@ -1,3 +1,8 @@
 # rk3399
 gst-launch-1.0 v4l2src device=/dev/video0 io-mode=4 ! videoconvert ! video/x-raw,format=NV12,width=640,height=480 ! kmssink
 sudo v4l2-ctl --list-devices
+
+# вывести дерево
+lsusb -t
+# посмотреть подробно
+lsusb -s 01:5 -v
