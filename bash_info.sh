1f0dc8666f7cb1725145be46e2d63c586a0dae78
exitstop <exitstop@list.ru>
2020-05-27 16:51:15 +0300

    llvm version

--- bash_info.sh
+++ bash_info.sh
@@ -671,8 +671,12 @@ sudo dpkg -r $(dpkg-deb -f foo.deb Package)
 
 # загрузить cpu на 100%
 sudo apt-get install stress-ng
-stress-ng --cpu 6 --cpu-method matrixprod  --metrics-brief --perf -t 60Osudo apt-get install stress-ng
+stress-ng --cpu 6 --cpu-method matrixprod  --metrics-brief --perf -t 60O
+sudo apt-get install stress-ng
 
 # binary diff path бинарый дифф
 http://www.daemonology.net/bsdiff/
 sudo apt install bsdiff
+
+# scrot скриншон screenshot
+scrot MyScreenshot.png
--- info_build/build_tvm_nnvm_mali_rk3399.sh
+++ info_build/build_tvm_nnvm_mali_rk3399.sh
@@ -1,14 +1,19 @@
 # rk3399 mali nnvm tvm
+# Это запустить на rk3399
+# cmake .. -DOpenCL_LIBRARY=/usr/lib/aarch64-linux-gnu/libOpenCL.so.1
 
 sudo apt-get update
+
+#sudo apt-get autoremove --purge "llvm-8*"
 
 sudo apt install -y llvm-4.0
 sudo apt install -y scons
 sudo apt install -y libopenblas-dev
+sudo apt install -y ocl-icd-opencl-dev
 # не уверен что это обязательно
 sudo apt install -y liblapack* libblas* libopencv*
 sudo apt-get -y install git cmake build-essential g++-4.8 c++-4.8 
-sudo apt install -y g++-4.8
+sudo apt install -y g++-4.8 gcc-4.8
 sudo update-alternatives --config g++
 sudo update-alternatives --config gcc
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 10
@@ -26,14 +31,25 @@ git clone --recursive https://github.com/apache/incubator-mxnet.git
 
 # build nnvm/tvm
 cd nnvm/tvm
-make USE_OPENCL=1  LLVM_CONFIG=llvm-config-4.0 -j$(nproc)
+mkdir build
+cd build
+cmake ..
+make
+#make USE_OPENCL=1  LLVM_CONFIG=llvm-config-4.0 -j$(nproc)
 if [ $? -eq 0 ]; then echo -e "[\e[32mOK\e[0m] make tvm"; \
 else echo -e "[\e[31mERROR\e[0m] make tvm" >&2; exit 1; fi
+
+cd ../python
+sudo python setup.py install
+cd ../topi/python
+sudo python setup.py install
+
 cd nnvm
 make -j$(nproc)
 if [ $? -eq 0 ]; then echo -e "[\e[32mOK\e[0m] make nnvm"; \
 else echo -e "[\e[31mERROR\e[0m] make nnvm" >&2; exit 1; fi
 cd ..
+
 
 # build arm compute library
 cd ComputeLibrary
