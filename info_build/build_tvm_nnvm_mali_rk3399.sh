76e95c2b186a0842261b5f4afe6670dcef5276cd
exitstop <exitstop@list.ru>
2020-06-06 13:58:47 +0300

    rk3399

--- info_build/build_tvm_nnvm_mali_rk3399.sh
+++ info_build/build_tvm_nnvm_mali_rk3399.sh
@@ -16,8 +16,23 @@ sudo apt-get -y install git cmake build-essential g++-4.8 c++-4.8
 sudo apt install -y g++-4.8 gcc-4.8
 sudo update-alternatives --config g++
 sudo update-alternatives --config gcc
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 30
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 40
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 10
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt-get update
+sudo apt-get install python3.6
+sudo apt-get install python3-distutils
+sudo apt-get install python3-dev
+
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3.6 get-pip.py
+sudo -H pip3.6 install setuptools Cython
+
+sudo ln -s /usr/bin/llvm-config-4.0 /usr/bin/llvm-config
 
 git clone --recursive https://github.com/dmlc/nnvm.git
 git clone https://github.com/ARM-software/ComputeLibrary.git --branch v17.12
@@ -40,11 +55,11 @@ if [ $? -eq 0 ]; then echo -e "[\e[32mOK\e[0m] make tvm"; \
 else echo -e "[\e[31mERROR\e[0m] make tvm" >&2; exit 1; fi
 
 cd ../python
-sudo python setup.py install
+python3.6 setup.py install --user
 cd ../topi/python
-sudo python setup.py install
+python3.6 setup.py install --user
 
-cd nnvm
+cd ../../nnvm
 make -j$(nproc)
 if [ $? -eq 0 ]; then echo -e "[\e[32mOK\e[0m] make nnvm"; \
 else echo -e "[\e[31mERROR\e[0m] make nnvm" >&2; exit 1; fi
@@ -52,8 +67,8 @@ cd ..
 
 
 # build arm compute library
-cd ComputeLibrary
-scons Werror=1 neon=1 opencl=1 examples=1 os=linux arch=arm64-v8a embed_kernels=1 build=native -j4
+cd ../../../ComputeLibrary/
+scons Werror=0 neon=1 opencl=1 examples=1 os=linux arch=arm64-v8a embed_kernels=1 build=native -j4
 cp ../acl_test.cc .
 
 g++ acl_test.cc build/utils/*.o -O2 -std=c++11 -I. -Iinclude -Lbuild -larm_compute -larm_compute_graph -larm_compute_core -lOpenCL -o acl_test
@@ -65,5 +80,6 @@ cd incubator-mxnet
 make -j$(nproc) USE_OPENCV=0 USE_BLAS=openblas
 cd ..
 
-export PYTHONPATH=/home/user/Downloads/tvm-mali/nnvm/tvm/nnvm/python:/home/user/Downloads/tvm-mali/nnvm/tvm/python:/home/user/Downloads/tvm-mali/nnvm/tvm/topi/python:${PYTHONPATH}
-export LD_LIBRARY_PATH=/home/user/Downloads/tvm-mali/nnvm/tvm/build:/home/user/Downloads/tvm-mali/nnvm/tvm/nnvm/lib:${LD_LIBRARY_PATH}
+export NNVM_PATH=/home/nvidia/nnvim_test/nnvm/tvm/python
+export PYTHONPATH=$NNVM_PATH/tvm/nnvm/python:$NNVM_PATH/tvm/python:$NNVM_PATH/nnvm/tvm/topi/python:${PYTHONPATH}
+export LD_LIBRARY_PATH=$NNVM_PATH/tvm/build:$NNVM_PATH/tvm/nnvm/lib:${LD_LIBRARY_PATH}
