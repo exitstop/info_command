c87a5d9a43fe44643d5423269ced4884099bff2d
exitstop <exitstop@list.ru>
2020-06-15 00:56:36 +0300

    ripgrep

--- grep_info.sh
+++ grep_info.sh
@@ -1,2 +1,7 @@
 - Ограничить по длине вывода, если строчка выводится слишком большая
 `grep -r -E -o ".{0,10}maxFilesize.{0,10}" *`
+
+# https://github.com/BurntSushi/ripgrep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
+sudo apt-get install ripgrep
