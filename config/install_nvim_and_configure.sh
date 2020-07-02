99ae7738b5dfe661efb39c47c091d5db5d41fbc1
exitstop <exitstop@list.ru>
2020-07-02 19:47:02 +0300

    vim

--- config/install_nvim_and_configure.sh
+++ config/install_nvim_and_configure.sh
@@ -33,7 +33,7 @@ set -u
 sudo update-alternatives --install /usr/bin/ex ex "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/vi vi "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/view view "${CUSTOM_NVIM_PATH}" 110
-sudo update-alternatives --install /usr/bin/vim vim "${CUSTOM_NVIM_PATH}" 110
+#sudo update-alternatives --install /usr/bin/vim vim "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/vimdiff vimdiff "${CUSTOM_NVIM_PATH}" 110
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim \
