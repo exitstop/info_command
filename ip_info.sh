90407dfabf553c5ce05202d1aaa5800ccd671b8a
Bogdan Kolos <exitstop@list.ru>
2020-12-08 22:13:59 +0300

    ip

--- ip_info.sh
+++ ip_info.sh
@@ -5,3 +5,8 @@ https://unix.stackexchange.com/questions/210982/bind-unix-program-to-specific-ne
 nmcli connection
 sudo nmcli connection modify "Wired connection 1" ipv4.route-metric 102
 nmcli connection up "Wired connection 1"
+
+# Отключить eth0 навсегда
+# https://askubuntu.com/questions/427922/disable-network-permanently
+sudo vim /etc/network/interfaces
+iface eth0 inet manual
