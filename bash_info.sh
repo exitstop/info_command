f0f613f022a6bc90411518c35ce7d51fb0e12a09
Kolos Bogdan <exitstop@list.ru>
2021-02-25 13:12:49 +0300

    deb

--- bash_info.sh
+++ bash_info.sh
@@ -923,3 +923,5 @@ https://unix.stackexchange.com/questions/9819/how-to-find-out-from-the-logs-what
 # find only directory
 find . -type d | cut -c 3- | sed 's/^/!*/'
 
+# force deb
+sudo dpkg -i --force-overwrite pack.db
